924dd2a40a25cabf00252531a26d09bcede28280
Alejandro Ciatti <alejandroaciatti@gmail.com>
2024-07-04 21:03:37 -0300

    configure gh actions

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import elmPlugin from 'vite-plugin-elm';
 
 export default defineConfig({
+  base: '/elm-experiments/',
   root: 'src',
   publicDir: 'src/assets',
   plugins: [elmPlugin()],
   build: {
-    outDir: '../docs',
+    outDir: '../dist',
     emptyOutDir: true,
   }
 });
